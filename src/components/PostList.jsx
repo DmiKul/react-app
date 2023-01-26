f72b116245f8935b42759cce68ae3be5ceafa0c8
Dmitry <mr.crocodile567@gmail.com>
2023-01-26 16:38:15 +0300

    feat: add animation for deleting and adding posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PostItem from './PostItem'
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
 
 function PostList({posts, title, remove}) {
 	if (!posts.length) {
@@ -11,10 +12,20 @@ function PostList({posts, title, remove}) {
 	}
 	return (
 		<div>
-			<h1 style={{textAlign: 'center'}}>{title}</h1>
-      {posts.map((post, index) => 
-        <PostItem remove={remove} number={index + 1} post={post} key={post.id}></PostItem>
-      )}
+			<h1 style={{textAlign: 'center'}}>
+				{title}
+			</h1>
+			<TransitionGroup>
+				{posts.map((post, index) => 
+					<CSSTransition
+						key={post.id}
+						timeout={500}
+						classNames='post'
+					>
+						<PostItem remove={remove} number={index + 1} post={post} key={post.id}></PostItem>
+					</CSSTransition>
+				)}
+			</TransitionGroup>
 		</div>
 	)
 }
